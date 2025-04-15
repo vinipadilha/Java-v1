34c5a488b94225663011efc947f65ec9f1665867
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-04-15 12:18:50 -0300

    Adicionando comentários ex4

--- src/Ex4.java
+++ src/Ex4.java
@@ -7,6 +7,7 @@ public class Ex4 {
         ArrayList<Aluno> lista =  new ArrayList<>();
 
         lista.add(new Aluno("Vini", 18));
+        //O objeto é uma "cópia" da classe, com valores reais. Ele é criado a partir da classe e ocupa espaço na memória.
         lista.add(new Aluno("Igor", 19));
         lista.add(new Aluno("Eduardo", 20));
 
@@ -16,18 +17,27 @@ public class Ex4 {
     }
 }
 
+
+// a classe é o molde, a estrtura
+// define como os objetos vão ser e oque vão saber fazer
 class Aluno {
+
+    //atributos
     String nome;
     int idade;
 
+
+    // construtor
     public Aluno(String nome, int idade){
         this.nome = nome;
         this.idade = idade;
         System.out.println("Aluno criado");
     }
 
+
+    //metodo
     public void apresentar(){
-        System.out.println("Olá, meu nome é " +nome+" e tenho " +  idade + " anos.");
+        System.out.println("Olá, meu nome é " + nome + " e tenho " + idade + " anos.");
 
     }
 
